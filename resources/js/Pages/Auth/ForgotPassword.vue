d30729d21d1c550e814eea8605e0de13fe7749e5
Ahmad-N-Mhala <ahmad.n.mhala@gmail.com>
2026-01-24 15:25:59 +0400

    Update support phone number in Forgot Password page

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -93,11 +93,11 @@
                     </svg>
                     <span dir="ltr">support@kenildock.com</span>
                 </a>
-                <a href="tel:+9715049460976" class="flex items-center gap-2 hover:text-primary transition-colors">
+                <a href="tel:+971504946097" class="flex items-center gap-2 hover:text-primary transition-colors">
                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                     </svg>
-                    <span dir="ltr">+971 50 494 60976</span>
+                    <span dir="ltr">+971 50 494 6097</span>
                 </a>
             </div>
         </div>
