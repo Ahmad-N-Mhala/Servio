4a3ef44a87d18e35028738470c1a1cbe8da763e3
Ahmad-N-Mhala <ahmad.n.mhala@gmail.com>
2026-02-10 21:06:50 +0400

    UI Fix: Prevent toggle switch distortion in RTL and ensure internal LTR mechanics

--- resources/js/Pages/Communication/Index.vue
+++ resources/js/Pages/Communication/Index.vue
@@ -146,7 +146,7 @@
                      <div class="flex items-center gap-4 p-4 bg-gray-50 rounded-xl border border-gray-200">
                         <label class="relative inline-flex items-center cursor-pointer">
                             <input type="checkbox" v-model="feedbackForm.is_active" class="sr-only peer" :disabled="!hasFeature('feedback')">
-                            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+                            <div dir="ltr" class="w-11 h-6 flex-shrink-0 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
                             <span class="ms-3 text-sm font-medium text-gray-900">{{ $t('communication.enable_feedback') }}</span>
                         </label>
                      </div>
